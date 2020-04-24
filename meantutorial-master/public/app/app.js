5e5276c756467fdedf3382ba3ccd90038aeaa1a0
akhilkabra <35921876+akhilkabra@users.noreply.github.com>
2020-04-24 17:20:00 +0530

    Add files via upload

---
+++ meantutorial-master/public/app/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('userApp',
+    [
+        'appRoutes',
+        'userControllers',
+        'userServices',
+        'mainController',
+        'authServices'
+    ])
+
+    .config(function($httpProvider){
+        $httpProvider.interceptors.push('AuthInterceptors');
+    });
---
+++ meantutorial-master/server.js
@@ -0,0 +1,33 @@
+var express 	= require('express');
+var app 		= express();
+const port 		= 8000 || process.env.PORT;
+var morgan 		= require('morgan');
+var mongoose 	= require('mongoose');
+var bodyParser	= require('body-parser');
+var router		= express.Router();
+var appRoutes 	= require('./app/routes/api')(router);
+var path		= require('path');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use('/api',appRoutes);
+//http://localhost:8000/api/users
+
+mongoose.connect('mongodb+srv://akhilkabra:Akhil1997@cluster0-ajgct.mongodb.net/test?retryWrites=true&w=majority',function(err){
+	if(err){
+		console.log("failed to connect to database: " + err);
+	}
+	else
+	{
+		console.log("Successfully connected to MongoDB");
+	}
+});
+
+app.get('*', function (req,res) {
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'))
+})
+app.listen(port ,function() {
+	console.log("Running server at port : ",port);
+}); 
